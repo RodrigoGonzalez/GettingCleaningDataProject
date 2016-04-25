b9d9ac0124a64eab43e61be2548e7bec37ac8e75
RodrigoGonzalez <rodrigo.gonzalez@colorado.edu>
2016-04-24 19:54:16 -0600

    R file updates

--- run_analysis.r
+++ run_analysis.r
@@ -1,6 +1,7 @@
 require(knitr)
 require(markdown)
-# setwd("~/GitHub repositories/GettingAndCleaningData/Project")
-setwd("~/Repositories/Coursera/GettingAndCleaningData/Project")
+
+#Set the working directory
+setwd("~/Dropbox/DataCourses/John Hopkins Univerisity Data Science/Getting and Cleaning Data/GettingCleaningDataProject")
 knit("run_analysis.Rmd", encoding="ISO8859-1")
 markdownToHTML("run_analysis.md", "run_analysis.html")
